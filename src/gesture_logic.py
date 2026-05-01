86845ce0dd67e8639e5812d982baf185537fdf44
ameypuranik2199 <amey.puranik21@gmail.com>
2026-05-01 19:57:15 +0530

    Delete src directory

--- src/gesture_logic.py
+++
@@ -1,7 +0,0 @@
-# Convert hand positions to steering & speed
-
-def calculate_steering(mid_x, screen_center):
-    return (mid_x - screen_center) / screen_center
-
-def calculate_speed(distance, min_d, max_d):
-    return (distance - min_d) / (max_d - min_d)
